fc64a1e07aa8d0c29e33268221b1596d33571cde
Jaroslav Kubacek <jaroslav.kubacek@oriflame.com>
2023-02-28 00:27:57 +0100

    Initial code

---
+++ CSTraining/CS6/ExceptionFiltering.cs
@@ -0,0 +1,28 @@
+﻿using static System.Net.HttpStatusCode;
+namespace CS6
+{
+
+    public class ExceptionFiltering
+    {
+        public static async Task<string> Test(string url)
+        {
+            using var client = new HttpClient();
+            try
+            {
+                return await client.GetStringAsync(url);
+            }
+            catch (HttpRequestException e) when (e.StatusCode == Moved)
+            {
+                return "Site moved";
+            }
+            catch (HttpRequestException e) when ((e.StatusCode == NotFound))
+            {
+                return "Not found";
+            }
+            catch (Exception ex)
+            {
+                return "Some other error";
+            }
+        }
+    }
+}
---
+++ CSTraining/CS6/StaticImport.cs
@@ -0,0 +1,17 @@
+﻿namespace CS6
+{
+    using static Math;
+    using static Console;
+    public class StaticImport
+    {
+        public static void Test()
+        {
+            var ran = new Random();
+            var a = ran.Next();
+            var b = ran.Next();
+            var c = Max(a, b);
+            WriteLine(c);
+            WriteLine(PI);
+        }
+    }
+}
